93b9809535e5652440a6d41d4b2d3d6e137582e1
misshanya <mishanyadev@gmail.com>
2025-02-12 18:39:36 +0300

    improve env variables getting

--- solution/config/config.go
+++ solution/config/config.go
@@ -15,11 +15,19 @@ type Config struct {
 func NewConfig() *Config {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		log.Printf("Error loading .env file: %v", err)
+		log.Println("Trying to get variables from environment...")
 	}
 
 	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatalln("missing DATABASE_URL env variable")
+	}
 	serverAddress := os.Getenv("SERVER_ADDRESS")
+	if serverAddress == "" {
+		log.Println("missing SERVER_ADDRESS env variable, using default (127.0.0.1:8080)")
+		serverAddress = "127.0.0.1:8080"
+	}
 
 	return &Config{
 		DatabaseURL:   dbURL,
